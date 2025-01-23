f0cd84bb7a3126163f4f64501865213d226d5194
kamal <kamal.zyel@gmail.com>
2025-01-23 15:06:17 +0700

    generate signature livewire

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,32 @@ class AppServiceProvider extends ServiceProvider
    */
   public function register(): void
   {
-    //
+    UrlGenerator::macro(
+      'alternateHasCorrectSignature',
+      function (Request $request, $absolute = true, array $ignoreQuery = []) {
+        $ignoreQuery[] = 'signature';
+
+        $absoluteUrl = url($request->path());
+        $url = $absolute ? $absoluteUrl : '/' . $request->path();
+
+        $queryString = collect(explode('&', (string) $request
+          ->server->get('QUERY_STRING')))
+          ->reject(fn($parameter) => in_array(Str::before($parameter, '='), $ignoreQuery))
+          ->join('&');
+
+        $original = rtrim($url . '?' . $queryString, '?');
+        $signature = hash_hmac('sha256', $original, call_user_func($this->keyResolver));
+        return hash_equals($signature, (string) $request->query('signature', ''));
+      }
+    );
+    UrlGenerator::macro('alternateHasValidSignature', function (Request
+    $request, $absolute = true, array $ignoreQuery = []) {
+      return URL::alternateHasCorrectSignature($request, $absolute, $ignoreQuery)
+        && URL::signatureHasNotExpired($request);
+    });
+    Request::macro('hasValidSignature', function ($absolute = true, array $ignoreQuery = []) {
+      return URL::alternateHasValidSignature($this, $absolute, $ignoreQuery);
+    });
   }
 
   /**
